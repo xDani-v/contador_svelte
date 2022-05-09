c9f25dcca5fe7bb01766915e0ccb754a13b051c3
Daniel Villano <84256368+xDani-v@users.noreply.github.com>
2022-05-09 08:17:12 -0500

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,24 @@
+<script>
+	let cursos = ['html','css','html'];
+	let numeros = [2,3];
+	const agg_numero  = () =>{
+		let num = Math.floor(Math.random() * (20 - 1)) + 1;
+		numeros = [...numeros,num];
+	}
+</script>
+
+<main class="container">
+	<h1>Cursos</h1>
+	<ul>
+		{#each cursos as curso, index}
+			<li>{index + 1}. {curso}</li>
+		{/each}
+	</ul>
+	<button on:click={agg_numero} class="btn btn-primary">Agregar numero</button>
+	<ul>
+		{#each numeros as numero}
+			 <li>{numero}</li>
+		{/each}
+	</ul>
+</main>
+
